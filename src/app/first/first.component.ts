eb6d05965da86fa9ff5b053141fc1ea9fdeac704
Akasha Sahu <akashasahu2001@gmail.com>
2025-02-01 22:49:06 +0530

    Update main

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-first',
   imports: [],
   templateUrl: './first.component.html',
-  styleUrl: './first.component.css'
+  styleUrl: './first.component.css',
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class FirstComponent {
 
